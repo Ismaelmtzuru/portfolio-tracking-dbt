e4336202bb85a1c6f8b9040f3c83e43f774bb6cd
Ismael urueta <Ismamtz4@gmail.com>
2025-09-25 01:56:13 +0000

    delpy

--- models/POSITION_ABC_BANK.sql
+++ models/POSITION_ABC_BANK.sql
@@ -1,2 +1,3 @@
-select * , position_value - cost_base as unrealized_profit
+select * , position_value - cost_base as unrealized_profit,
+ROUND((POSITION_VALUE - COST_BASE) / COST_BASE, 5) as unrealized_profit_pct
 from {{source('abc_bank', 'ABC_BANK_POSITION')}}
